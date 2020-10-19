0617798cd63a6ba47539e4a014f694e03175734a
moe-idris <73137000+moe-idris@users.noreply.github.com>
2020-10-20 00:08:15 +0200

    Update Measuing_current.ino

--- Measuing_current.ino
+++ Measuing_current.ino
@@ -1,9 +1,7 @@
 // Program to measure consumed current by a load with the SCT013-30A current sensor
 
-/***** Include constants for the Pin where the Sensor is connected to *******/ 
-/*****____________WRITE HERE THE CODE_________________*******/
-const int SensorPin = A0 , RefPin = A2;
-const int Rshunt = 33 ;
+const int SensorPin = A0 , RefPin = A2; //definition of the arduino sensor and reference pin constants
+const int Rshunt = 33 ; //shunt resistance value
 
 // Definition of auxiliary variables 
 unsigned long time_ant = 0, difTime = 0, act_time = 0;
@@ -12,22 +10,16 @@ int count_integral = 0;
 double rawSquaredSum = 0;
 double Iant = 0;
 
-/***** Include constants for the grid frecuency and current transformer properties *******/
-/*****____________WRITE HERE THE CODE_________________*******/
-// Definition of constant grid frequency (50 Hz)
-double freq = 50 ;
-// Definition of transformer reduction relationship, you can check it in the datasheet
-double n_trafo = 1000;
+double freq = 50 ; //definition of the grid frequency (50 Hz in Europe)
+
+double n_trafo = 1000; //definition of the transformer turn ratio determined from the SCT013 current sensor datasheet
 
 // Definition of measured current variable 
 double Irms = 0;
 
-/***** Write the void setup code command. Inside it, Set the data rate in bits per second for serial data transmission, 
-so as to communicate with the computer. Use of the availables reates *******/
-/*****____________WRITE HERE THE CODE_________________*******/
-void setup() {
-  // Initialize the serial port communications with a baudrate of 115200 bit/s
-  Serial.begin(115200);
+/****For the sake of data transmission from the arduino to the computer, 
+  the baud rate is defined using the following command. In this case the baud rate is set to 115200 bits/s****/
+  Serial.begin(115200); 
 
 
 }
@@ -43,25 +35,24 @@ void loop()
   if (difTime >= 1000) {
     time_ant = act_time + (difTime - 1000);
 
-    /***** Read the ADC (Analog Digital convertor) input from the sensor and the voltage reference point *******/
-    /*****____________WRITE HERE THE CODE_________________*******/
+    /***** The ADC (Analog Digital convertor) input from the sensor and the voltage reference point *****/
     /* We got the value in bits and the relation is 5V - 1023 bits*/
-     double ADC_sensor = analogRead(A0);
-     double ADC_ref = analogRead(A2);
+    
+     double ADC_sensor = analogRead(A0); //definition of the sensor pin in the arduino that is to be read and converted into digital information
+     double ADC_ref = analogRead(A2); //definition of the voltage reference pin in the arduino that is to be read and converted into digital information
 
     
-    /*****  Translate the ADC input measured to voltage values *******/
-    /*****____________WRITE HERE THE CODE_________________*******/
-    double V_sens = ADC_sensor*5/1023;
+    /***** The measured ADC inputs of the sensor and reference point are translated into voltage values using the following formula *******/
+    double V_sens = ADC_sensor*5.0/1023;
     double V_ref = ADC_ref*5.0/1023;
 
-    // Calculate the instantaneous current using the voltage diference and the burder resistor value
+    // Instantaneous current is then calculated by using the voltage difference, shunt resistance value, and transformer turn ratio value as follows//
     double Iinst =  n_trafo * (V_sens - V_ref) / Rshunt;
       
-    // Print the instantaneous current
-    //Serial.println(Iinst);
+    // The instantaneous current (calculated above) is then printed using the following command//
+    Serial.println(Iinst);
     
-    // Calculate the integral
+    // The integral in the RMS current equation is calculated as follows//
     rawSquaredSum += Iinst * Iinst * 0.001;
     
     // Count 20 ms
@@ -71,18 +62,20 @@ void loop()
   // Each 20 ms, calculte the RMS
   if (count_integral >= sampleDuration) 
   {
-    // Calculate the RMS
+    /***The RMS current is then calculated due to the fact that the instantaneous current is not useful in determining the mean current consumption of the kettle.
+    This is mainly because the input current itself is sinusoidal (alternating current) ***/
+ 
     Irms = sqrt(freq * rawSquaredSum);
-    // Counter and integral reset
+    
+    // Counter and integral reset//
     count_integral = 0;
     rawSquaredSum = 0;
 
-    // Low-pass filter
+    // Low-pass filter//
     double Ifilt = 0.8 * Iant + 0.2 * Irms;
     Iant = Ifilt;
     
-    // Print the RMS
-   // Serial.print("Ifilt = ");
+    // RMS is then printed using the following command again //
    Serial.println(Ifilt);
   }
 }
